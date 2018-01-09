b44b5af3bd39a484134be4201d801340ac1c0d59
JoanNgatia <joan.ngatia@andela.com>
2018-01-09 16:34:22 +0300

    (chore) Add DRF and update requirements file

--- library/library/settings.py
+++ library/library/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # frameworks
+    'rest_framework',
     # internal apps
     'books',
 ]
